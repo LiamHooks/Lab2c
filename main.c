b7360d374185756af6adce12adb0baf42fd85a02
LiamHooks <lrh5428@psu.edu>
2020-09-08 15:18:47 +0000

    Initial commit

---
+++ main.c
@@ -0,0 +1,32 @@
+// Author: Liam Hooks lrh5428@psu.edu
+// Collaborator: Angela Bao ymb5072@psu.edu
+// Collaborator: Seulki Kim svk6271@psu.edu
+// Collaborator:
+// Section: 2
+// Breakout: 2
+#include <stdio.h>
+#include <readline/readline.h>
+#include <stdlib.h>
+
+int main(void) {
+  char *gradeInput = readline("Enter your CMPSC 131 grade: ");
+  double grade = strtod(gradeInput, NULL);
+  if (grade >= 93.0)
+    printf("Your letter grade for CMPSC 131 is A.");
+  else if (grade >= 90.0)
+    printf("Your letter grade for CMPSC 131 is A-.");
+  else if (grade >= 87.0)
+    printf("Your letter grade for CMPSC 131 is B+.");
+  else if (grade >= 83.0)
+    printf("Your letter grade for CMPSC 131 is B.");
+  else if (grade >= 80.0)
+    printf("Your letter grade for CMPSC 131 is B-.");
+  else if (grade >= 77.0)
+    printf("Your letter grade for CMPSC 131 is C+.");
+  else if (grade >= 70.0)
+    printf("Your letter grade for CMPSC 131 is C.");
+  else if (grade >= 60.0)
+    printf("Your letter grade for CMPSC 131 is D.");
+  else
+    printf("Your letter grade for CMPSC 131 is F.");
+}
